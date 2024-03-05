d263b2a30c9b97bca6fb52dab3f0185ce294dbbe
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2024-03-05 00:11:24 -0500

    Papi2

---
+++ sets-lab-main/disjuntos.py
@@ -0,0 +1,12 @@
+def disjuntos(conjuntos):
+    # Iteramos sobre todos los pares de conjuntos
+    for i in range(len(conjuntos)):
+        for j in range(i + 1, len(conjuntos)):
+            conjunto1 = conjuntos[i]
+            conjunto2 = conjuntos[j]
+            # Verificar si algún elemento del conjunto 1 está presente en el conjunto 2
+            for elemento in conjunto1:
+                if elemento in conjunto2:
+                    return "Los conjuntos no son disjuntos"
+                else:
+                    return "Los conjuntos son disjuntos"
